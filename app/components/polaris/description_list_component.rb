4b266cda129cd480a34d057135d8b6495b90dd91
Jay El-Kaake <najibkaake@gmail.com>
2025-07-07 20:49:19 -0400

    allow description lists to receive class names

--- app/components/polaris/description_list_component.rb
+++ app/components/polaris/description_list_component.rb
@@ -26,17 +26,19 @@ module Polaris
     end
 
     class DescriptionListItemComponent < Polaris::Component
-      def initialize(term:)
+      def initialize(term:, dt_classes: nil, dd_classes: nil)
         @term = term
+        @dt_classes = dt_classes
+        @dd_classes = dd_classes
       end
 
       def call
         safe_join [
-          tag.dt(class: "Polaris-DescriptionList__Term") do
-            render(Polaris::TextComponent.new(as: :span, variant: :headingSm).with_content(@term))
-          end,
-          tag.dd(class: "Polaris-DescriptionList__Description") { content }
-        ]
+                    tag.dt(class: "Polaris-DescriptionList__Term #{@dt_classes}".strip) do
+                      render(Polaris::TextComponent.new(as: :span, variant: :headingSm).with_content(@term))
+                    end,
+                    tag.dd(class: "Polaris-DescriptionList__Description #{@dd_classes}".strip) { content }
+                  ]
       end
     end
   end
